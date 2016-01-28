0eb04068fcc37c7382db17e74e42b98a5c7b840e
Looncall <stefanjentoft@gmail.com>
2016-01-28 17:22:14 -0500

    Added physics engine
    Arrow keys now accelerate the object rather than directly move it.
    Friction is now in play.  Bouncing off edges added, but needs to be
    perfected.

--- kong.py
+++ kong.py
@@ -10,19 +10,27 @@ class Kong:
 		self.key_states = key_states
 		self.kong_sprite = pyglet.sprite.Sprite(image, x = 50, y = 50)
 		self.kong_sprite.scale = 100/self.kong_sprite.width
+		self.dx = 100
+		self.dy = 100
 
 	def draw(self):
 		self.kong_sprite.draw()
 
 	def update(self, dt):
+		self.kong_sprite.x += self.dx * dt
+		self.kong_sprite.y += self.dy * dt
+
+		self.dx *= 0.99
+		self.dy *= 0.99
+
 		if self.key_states[key.UP]:
-			self.kong_sprite.y += self.SPEED*dt
+			self.dy += self.SPEED*dt
 		if self.key_states[key.DOWN]:
-			self.kong_sprite.y -= self.SPEED*dt
+			self.dy -= self.SPEED*dt
 		if self.key_states[key.LEFT]:
-			self.kong_sprite.x -= self.SPEED*dt
+			self.dx -= self.SPEED*dt
 		if self.key_states[key.RIGHT]:
-			self.kong_sprite.x += self.SPEED*dt
+			self.dx += self.SPEED*dt
 
 		min_y = 0
 		max_y = self.window.height - self.kong_sprite.height
@@ -31,9 +39,13 @@ class Kong:
 
 		if self.kong_sprite.y < min_y:
 			self.kong_sprite.y = min_y
+			self.dy *= -1
 		if self.kong_sprite.y > max_y:
 			self.kong_sprite.y = max_y
+			self.dy *= -1
 		if self.kong_sprite.x < min_x:
 			self.kong_sprite.x = min_x
+			self.dx *= -1
 		if self.kong_sprite.x > max_x:
 			self.kong_sprite.x = max_x
+			self.dx *= -1
