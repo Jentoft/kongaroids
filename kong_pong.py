4c36c0db0beabc93ecb212e12e001ab2e48d15af
Looncall <stefanjentoft@gmail.com>
2016-02-17 21:58:50 -0500

    Beginnings of collision detection for kong pong
    Added a detect_collisions function that returns which two objects are
    colliding if any, or an empty set if no objects are currently colliding.
    
    I also added headers for functions to determine whether kong has
    collided with either paddle, but still need to implement them.

--- kong_pong.py
+++ kong_pong.py
@@ -14,6 +14,22 @@ kong = Kong(window, key_states, kong_image)
 paddleA = Paddle(window, key_states, paddle_image, 'A')
 paddleB = Paddle(window, key_states, paddle_image, 'B')
 
+def a_collision_detected():
+	pass
+
+def b_collision_detected():
+	pass
+
+def detect_collisions(objects):
+	if a_collision_detected():
+		return [objects[1], objects[3]]
+	elif b_collision_detected():
+		return [objects[2], objects[3]]
+	else:
+		return []
+
+
+
 @window.event
 def on_draw():
 	window.clear()
@@ -25,6 +41,7 @@ def update(dt):
 	a_position = paddleA.update()
 	b_position = paddleB.update()
 	kong.update(dt, a_position, b_position)
+	detect_collisions([paddleA, paddleB, kong])
 
 pyglet.clock.schedule_interval(update, 1/120)
 pyglet.app.run()
